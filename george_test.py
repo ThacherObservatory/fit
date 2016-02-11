19e34a344a4be8522eb381c7edc512e75cfdd09b
jswift <jswift@thacher.org>
2016-02-10 23:12:48 -0800

    Added emcee fitter for george_test. Looks like we will need to think about priors a bit (at least for the dataset that we are using)

--- george_test.py
+++ george_test.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import scipy.optimize as op
 import kplr
 import emcee
+import pdb
 
 client = kplr.API()
 
@@ -43,48 +44,118 @@ plt.plot(time,flux,'ko-',markersize=8)
 #plt.xlabel('Time (BKJD)')
 plt.ylabel('Flux (ADU)')
 
-k =  9.8 * ExpSquaredKernel(1) * ExpSine2Kernel(1,.5)
+k =  14**2 * ExpSquaredKernel(0.8) * ExpSine2Kernel(1.2,0.7)
 
 gp = george.GP(k,mean=np.mean(flux))
-
-def lnprob(theta):
+def lnprob(theta,time,flux,err):
     #theta[0] = amplitude
     #theta[1] = width
     #theta[2] = width of semi periodic kernel
     #theta[3] = period
 
+#    theta is actually the natural log of the input parameters!
     gp.kernel[:] = theta
 
-    
+#    print(np.exp(theta))
+#    if np.exp(theta[0]) <= 0 or np.exp(theta[0]) > 1000:
+#        return np.inf
+#
+#    if theta[1] < 0 or theta[1] > 20:
+#        return np.inf
+#
+#    if np.exp(theta[2] < 0 or theta[2] > 20:
+#        return np.inf
+#
+#    if theta[3] < 0 or theta[3] > 20:
+#        return np.inf
+   
     try:
         gp.compute(time,4,sort=True)
     except (ValueError, np.linalg.LinAlgError):
         print('WTF!')
         return np.inf
 
-    return gp.lnlikelihood(flux, quiet=True)
+    loglike = gp.lnlikelihood(flux, quiet=True)
+    loglike -= 0.5*((np.exp(theta[1])-10)/2)**2
+    loglike -= 0.5*((np.exp(theta[2])-0.5)/0.1)**2
+    loglike -= 0.5*((np.exp(theta[3])-1)/0.5)**2
+    
+    return loglike #gp.lnlikelihood(flux, quiet=True)
 
 gp.compute(time,4,sort=True)
 print(gp.lnlikelihood(flux))
 
 p0 = gp.kernel.vector
-nwalkers = 100
-nsteps = 1000
+nwalkers = 20
+burnsteps = 500
+mcmcsteps = 500
 ndim = len(p0)
-#drop the MCMC hammer
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
+# drop the MCMC hammer, yo.
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(time,flux,err))
 
-pos,prob,state = sampler.run_mcmc(p0, nsteps)
+p0_vec = [np.abs(p0[i])+1e-3*np.random.randn(nwalkers) for i in range(ndim)]
+p0_init = np.array(p0_vec).T
+
+pos,prob,state = sampler.run_mcmc(p0_init, burnsteps)
+
+plt.figure(2)
+plt.clf()
+for i in range(nwalkers):
+    plt.subplot(2,2,1)
+    plt.plot(sampler.chain[i,:,0])
+    plt.title('Log Amp')
+for i in range(nwalkers):
+    plt.subplot(2,2,2)
+    plt.plot(np.exp(sampler.chain[i,:,1]))
+    plt.title('Sine Amp')
+for i in range(nwalkers):
+    plt.subplot(2,2,3)
+    plt.plot(np.exp(sampler.chain[i,:,2]))
+    plt.title('Period')
+for i in range(nwalkers):
+    plt.subplot(2,2,4)
+    plt.plot(sampler.chain[i,:,3])
+    plt.title('Decay')
+plt.suptitle('Burn-in',fontsize=18)
 
 sampler.reset()
-pos, prob, state = sampler.run_mcmc(pos,1000)
+pos, prob, state = sampler.run_mcmc(pos,mcmcsteps)
 
+plt.figure(3)
+plt.clf()
+for i in range(nwalkers):
+    plt.subplot(2,2,1)
+    plt.plot(sampler.chain[i,:,0])
+    plt.title('Log Amp')
+for i in range(nwalkers):
+    plt.subplot(2,2,2)
+    plt.plot(np.exp(sampler.chain[i,:,1]))
+    plt.title('Sine Amp')
+for i in range(nwalkers):
+    plt.subplot(2,2,3)
+    plt.plot(np.exp(sampler.chain[i,:,2]))
+    plt.title('Period')
+for i in range(nwalkers):
+    plt.subplot(2,2,4)
+    plt.plot(sampler.chain[i,:,3])
+    plt.title('Decay')
+
+plt.suptitle('Final',fontsize=18)
+
+logamp = np.median(sampler.flatchain[:,0])
+logg = np.median(sampler.flatchain[:,1])
+logp = np.median(sampler.flatchain[:,2])
+logw = np.median(sampler.flatchain[:,3])
+
+fit =  np.array([logamp,logg,logp,logw])
+gp.kernel[:] = fit
 print(gp.lnlikelihood(flux))
 
 x = np.linspace(np.min(time), np.max(time), 5000)
 gp.compute(time,4,sort=True)
 mu, cov = gp.predict(flux, x)
+plt.figure(1)
 plt.plot(x,mu,'r-')
 
 flux_fit, cov_fit = gp.predict(flux,time)
@@ -93,10 +164,11 @@ flux_fit, cov_fit = gp.predict(flux,time)
 #plt.xlim(355.7,355.8)
 #plt.ylim(14500,14600)
 
+plt.figure(1)
 plt.subplot(2,1,2)
 plt.plot(time,flux_fit-flux,'ko')
 plt.axhline(y=0,linestyle='-',color='red',lw=3)
 plt.xlabel('Time (BKJD)')
 plt.ylabel('Residuals (ADU)')
 
-plt.savefig('GP_4175707.png',dpi=300)
+#plt.savefig('GP_4175707.png',dpi=300)
