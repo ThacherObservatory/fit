Fixed bug with sign of lnprob, added strong priors and now MCMC is working decently well. Still an issue with the scatter in the residuals increasing in time. Not sure what is going on there.

george_test.py
@@ -11,9 +11,9 @@ debug = False
 
 client = kplr.API()
 
-#star = client.star(10935310)
+star = client.star(10935310)
 #star = client.star(4175707)
-star = client.star(11913210)
+#star = client.star(11913210)
 
 print('Get LCs...')
 lcs = star.get_light_curves(fetch=True)
@@ -35,26 +35,38 @@ for lc in lcs:
     errs =  np.append(errs,e)
 
 
-#t0 =1
-#period =1 
+t0 = 2454957.32211 - 2454833.0
+period =4.1287977964 
 #phase = (times - t0) % period
 # Use quasi-periodic kernel -- follow example
 f = (np.isfinite(times)) & (np.isfinite(fluxes)) & (np.isfinite(errs))
-#i = np.where((times[f] > 540) & (times[f] < 570))
+i = np.where((times[f] > 540) & (times[f] < 570))
 #i = np.where((times[f] > 353) & (times[f] < 360))
-i = np.where((times[f] > 292) & (times[f] < 300))
+#i = np.where((times[f] > 292) & (times[f] < 300))
 time = times[f][i] ; flux = fluxes[f][i] ; err = errs[f][i]
+bad, = np.where((time>295.5) & (time<295.6))
+bad.tolist()
+time = np.delete(time,bad)
+flux = np.delete(flux,bad)
+err = np.delete(err,bad)
+
+phase = ((time - t0) % period)/period
+ooei1, = np.where((phase > 0.02) & (phase < 0.48))
+ooei2, = np.where((phase > 0.52) & (phase < 0.98))
+ooei = np.sort(np.append(ooei1,ooei2))
+time = time[ooei] ; flux = flux[ooei]; err=err[ooei]
+
 
 plt.ion()
 plt.figure(1)
 plt.clf()
 plt.subplot(2,1,1)
-plt.plot(time,flux,'ko-',markersize=8)
-#plt.xlabel('Time (BKJD)')
+plt.plot(time,flux,'ko',markersize=2)
+plt.xlabel('Time (BKJD)')
 plt.ylabel('Flux (ADU)')
 
-#k =  14**2 * ExpSquaredKernel(10) * ExpSine2Kernel(2,3)
-k =  200**2 * ExpSquaredKernel(0.5)
+k =  100**2 * ExpSquaredKernel(1) * ExpSine2Kernel(4,4)
+#k =  300**2 * ExpSquaredKernel(2)
 gp = george.GP(k,mean=np.mean(flux))
 
 if debug:
@@ -69,9 +81,10 @@ if debug:
     plt.axhline(y=0,linestyle='-',color='red',lw=3)
     plt.xlabel('Time (BKJD)')
     plt.ylabel('Residuals (ADU)')
+    plt.suptitle('KIC 10935310 (eclipses blanked)')
+    plt.savefig('KIC10935310_oee.png',dpi=300)
     sys.exit()
     
-
 
 
 def lnprob(theta,time,flux,err):
@@ -81,45 +94,46 @@ def lnprob(theta,time,flux,err):
     #theta[3] = period
 
 #    theta is actually the natural log of the input parameters!
-    gp.kernel[:] = theta
+    gp.kernel[:] = np.array([theta[0],theta[1],0.5,theta[2]])
 
 #    print(np.exp(theta))
 #    if np.exp(theta[0]) <= 0 or np.exp(theta[0]) > 1000:
-#        return np.inf
+#        return -np.inf
 #
-#    if theta[1] < 0 or theta[1] > 20:
-#        return np.inf
+    if np.exp(theta[1]) < 0.75  or np.exp(theta[1]) > 1.25:
+        return -np.inf
 #
-#    if np.exp(theta[2] < 0 or theta[2] > 20:
-#        return np.inf
+    if np.exp(theta[2]) < 3 or np.exp(theta[2]) > 5:
+        return -np.inf
 #
-#    if theta[3] < 0 or theta[3] > 20:
-#        return np.inf
+    if np.exp(theta[3]) < 2 or np.exp(theta[3]) > 6:
+        return -np.inf
    
     try:
         gp.compute(time,4,sort=True)
-        gp.compute(time,2,sort=True)
+#        gp.compute(time,2,sort=True)
     except (ValueError, np.linalg.LinAlgError):
 #        print('WTF!')
-        return np.inf
+        return -np.inf
 
     loglike = gp.lnlikelihood(flux, quiet=True)
-    loglike -= 0.5*((np.exp(theta[1])-0.5)/0.01)**2
-    if np.exp(theta[1]) <= 0.3:
-        return np.inf
-    #    loglike -= 0.5*((np.exp(theta[2])-2)/0.1)**2
-#    loglike -= 0.5*((np.exp(theta[3])-3)/1)**2
+#    loglike -= 0.5*((np.exp(theta[1])-2)/0.01)**2
+#    if np.exp(theta[1]) <= 0.3:
+#        return -np.inf
+#    loglike -= 0.5*((np.exp(theta[2])-2)/0.1)**2
+#    loglike -= 0.5*((np.exp(theta[3])-0.5)/0.1)**4
     
     return loglike #gp.lnlikelihood(flux, quiet=True)
 
-#gp.compute(time,4,sort=True)
-gp.compute(time,2,sort=True)
+gp.compute(time,4,sort=True)
+#gp.compute(time,2,sort=True)
 print(gp.lnlikelihood(flux))
 
 p0 = gp.kernel.vector
+#p0 = p0[0:3]
 nwalkers = 20
-burnsteps = 200
-mcmcsteps = 200
+burnsteps = 1000
+mcmcsteps = 1000
 ndim = len(p0)
 
 # drop the MCMC hammer, yo.
@@ -139,15 +153,15 @@ for i in range(nwalkers):
 for i in range(nwalkers):
     plt.subplot(2,2,2)
     plt.plot(np.exp(sampler.chain[i,:,1]))
-#    plt.title('Sine Amp')
-#for i in range(nwalkers):
-#    plt.subplot(2,2,3)
-#    plt.plot(np.exp(sampler.chain[i,:,2]))
-#    plt.title('Period')
-#for i in range(nwalkers):
-#    plt.subplot(2,2,4)
-#    plt.plot(sampler.chain[i,:,3])
-#    plt.title('Decay')
+    plt.title('Sine Amp')
+for i in range(nwalkers):
+    plt.subplot(2,2,3)
+    plt.plot(np.exp(sampler.chain[i,:,2]))
+    plt.title('Period')
+for i in range(nwalkers):
+    plt.subplot(2,2,4)
+    plt.plot(sampler.chain[i,:,3])
+    plt.title('Decay')
 plt.suptitle('Burn-in',fontsize=18)
 
 sampler.reset()
@@ -162,30 +176,30 @@ for i in range(nwalkers):
 for i in range(nwalkers):
     plt.subplot(2,2,2)
     plt.plot(np.exp(sampler.chain[i,:,1]))
-#    plt.title('Sine Amp')
-#for i in range(nwalkers):
-#    plt.subplot(2,2,3)
-#    plt.plot(np.exp(sampler.chain[i,:,2]))
-#    plt.title('Period')
-#for i in range(nwalkers):
-#    plt.subplot(2,2,4)
-#    plt.plot(sampler.chain[i,:,3])
-#    plt.title('Decay')
+    plt.title('Sine Amp')
+for i in range(nwalkers):
+    plt.subplot(2,2,3)
+    plt.plot(np.exp(sampler.chain[i,:,2]))
+    plt.title('Period')
+for i in range(nwalkers):
+    plt.subplot(2,2,4)
+    plt.plot(sampler.chain[i,:,3])
+    plt.title('Decay')
 plt.suptitle('Final',fontsize=18)
 
 logamp = np.median(sampler.flatchain[:,0])
 logg = np.median(sampler.flatchain[:,1])
-#logp = np.median(sampler.flatchain[:,2])
-#logw = np.median(sampler.flatchain[:,3])
+logp = np.median(sampler.flatchain[:,2])
+logw = np.median(sampler.flatchain[:,3])
 
-#fit =  np.array([logamp,logg,logp,logw])
-fit =  np.array([logamp,logg])
+fit =  np.array([logamp,logg,logp,logw])
+#fit =  np.array([logamp,logg])
 gp.kernel[:] = fit
 print(gp.lnlikelihood(flux))
 
 x = np.linspace(np.min(time), np.max(time), 5000)
-#gp.compute(time,4,sort=True)
-gp.compute(time,2,sort=True)
+gp.compute(time,4,sort=True)
+#gp.compute(time,2,sort=True)
 mu, cov = gp.predict(flux, x)
 plt.figure(1)
 plt.plot(x,mu,'r-')