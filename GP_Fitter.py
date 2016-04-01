45e99a7141809fda7134e18dd2e5d5c35b44cad6
jswift <jswift@thacher.org>
2016-03-31 22:59:17 -0700

    Playing around with GP_Fitter.py.

--- GP_Fitter.py
+++ GP_Fitter.py
@@ -7,6 +7,7 @@ import emcee, george, corner
 from george.kernels import ExpSine2Kernel, ExpSquaredKernel
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 time,flux,err = np.loadtxt("10935310_ooe_real.txt",unpack=True)
 
@@ -21,23 +22,31 @@ plt.ion()
 plt.figure(1)
 plt.clf()
 plt.subplot(2,1,1)
-plt.plot(time,flux,'ko',markersize=2)
+plt.plot(time,flux,'ko',markersize=5)
 plt.xlabel('Time (BKJD)')
 plt.ylabel('Flux (ADU)')
 
-
-k1 =  .02**2 * ExpSquaredKernel(1) * ExpSine2Kernel(4,.002)
-k2 =  .02**2 * ExpSquaredKernel(1) * ExpSine2Kernel(4,.002)
+#test: effects of combining two identical kernels?
+# Yes, one can be attributed to one star, and one from the other.
+# Don't know if it will work, but it is similar to Ben's choice to
+# make the amplitude of the semi-periodic kernel semi-periodic.
+#note: gp.kernel.vector has length 8
+# I haven't tested it yet. Note, I'm only using k1 for now.
+k1 =  20**2 * ExpSquaredKernel(10) * ExpSine2Kernel(1.0,4)
+k2 =  .01**2 * ExpSquaredKernel(0.5) * ExpSine2Kernel(2,.001)
 k = k1 + k2
 gp = george.GP(k1,mean=np.mean(flux),solver=george.HODLRSolver)
+print(gp.kernel.vector)
 
+gp.compute(time,4,sort=True)
+flux_fit, cov_fit = gp.predict(flux, time)
 
-#test: effects of combining two identical kernels?
-#note: gp.kernel.vector has length 8
-#k1 =  .02**2 * ExpSquaredKernel(1) * ExpSine2Kernel(4,.002)
-#k2 =  .01**2 * ExpSquaredKernel(0.5) * ExpSine2Kernel(2,.001)
-#k = k1 + k2
-#print(gp.kernel.vector)
+#plot gp prediction
+plt.figure(1)
+plt.subplot(2,1,1)
+plt.plot(time,flux_fit,'g.')
+
+#sys.exit()
 
 def lnprob(theta,time,flux,err):
     """log likelihood function for MCMC"""
@@ -49,14 +58,18 @@ def lnprob(theta,time,flux,err):
     #theta is actually the natural log of the input parameters!
 
     #Note to Dr. Swift: what's going on here with theta 2 and 3?
-    gp.kernel[:] = np.array([theta[0],theta[1],0.5,theta[2]])
+    # I was experimenting with holding some parameters constant.
+    gp.kernel[:] = np.array([theta[0],theta[1],theta[2],theta[3]])
 
-    if np.exp(theta[1]) < 0.75  or np.exp(theta[1]) > 1.25:
-        return -np.inf
-    if np.exp(theta[2]) < 3 or np.exp(theta[2]) > 5:
-        return -np.inf
-    if np.exp(theta[3]) < .0005 or np.exp(theta[3]) > .005:
-        return -np.inf
+#    if np.exp(theta[0]) < 5.0  or np.exp(theta[0]) > 50.0:
+#        return -np.inf
+#    if np.exp(theta[1]) < 0.01  or np.exp(theta[1]) > 5:
+#        return -np.inf
+#    if np.exp(theta[2]) < 2 or np.exp(theta[2]) > 7:
+#        return -np.inf
+#    if np.exp(theta[3]) < 3 or np.exp(theta[3]) > 5:
+#        return -np.inf
+#    print gp.kernel.vector
    
     try:
         gp.compute(time,4,sort=True)
@@ -74,12 +87,13 @@ print(gp.lnlikelihood(flux))
 
 #Initialize the MCMC Hammer
 p0 = gp.kernel.vector
-nwalkers = 100
-burnsteps = 2000
-mcmcsteps = 2000
+nwalkers = 50
+burnsteps = 100
+mcmcsteps = 100
 ndim = len(p0)
-p0_vec = [np.abs(p0[i])+1e-3*np.random.randn(nwalkers) for i in range(ndim)]
+p0_vec = [p0[i]+1e-3*np.random.randn(nwalkers) for i in range(ndim)]
 p0_init = np.array(p0_vec).T
+
 
 #Drop the MCMC Hammer
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(time,flux,err))
@@ -103,6 +117,9 @@ logw = np.median(sampler.flatchain[:,3])
 fit =  np.array([logamp,logg,logp,logw])
 gp.kernel[:] = fit
 
+print(gp.kernel.vector)
+
+
 #report lnlikelihood post-MCMC
 print(gp.lnlikelihood(flux))
 
@@ -113,6 +130,7 @@ flux_fit, cov_fit = gp.predict(flux, time)
 
 #plot gp prediction
 plt.figure(1)
+plt.subplot(2,1,1)
 plt.plot(time,flux_fit,'r.')
 
 #plot residuals
@@ -122,9 +140,9 @@ plt.plot(time,flux_fit-flux,'ko')
 plt.axhline(y=0,linestyle='-',color='red',lw=3)
 plt.xlabel('Time (BKJD)')
 plt.ylabel('Residuals (ADU)')
-plt.savefig("10935310_ooe_fit.png",dpi=300)
+#plt.savefig("10935310_ooe_fit.png",dpi=300)
 
 #generate corner plot of gp parameters
 samples = sampler.chain.reshape((-1, ndim))
-figure = corner.corner(samples, labels=["$T1$","$T2$","$T3$","$T4$","$T5$"])
-figure.savefig("gp_test_corner.png",dpi=300)
+figure = corner.corner(samples, labels=["$T_1$","$T_2$","$T_3$","$T_4$"])
+#figure.savefig("gp_test_corner.png",dpi=300)
